f52bb9a0992e1f4dfd5a36340c4da3b5b99d77e9
Santiago Velez <santileo133@gmail.com>
2024-09-25 19:57:56 -0300

    todo victorycount

--- routes/avatar.routes.js
+++ routes/avatar.routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const avatarController = require('../controllers/avatar.controllers')
 const imgMulter = require('../middlewares/imgMulter')
-// const auth = require('../middlewares/auth')
-// const isAdmin = require('../middlewares/isAdmin')
+const auth = require('../middlewares/auth')
+const isAdmin = require('../middlewares/isAdmin')
 
 router.get('/avatar', avatarController.getAvatars)
-router.post('/avatar', imgMulter, avatarController.postAvatar)
-router.delete('/avatar/:id', avatarController.deleteAvatar)
+router.post('/avatar', [ imgMulter, auth, isAdmin ] , avatarController.postAvatar)
+router.delete('/avatar/:id', [ auth, isAdmin ] , avatarController.deleteAvatar)
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,14 @@ const router = require('express').Router()
 const userController = require('../controllers/user.controllers')
 const multer = require('multer')
 const upload = multer()
+const auth = require('../middlewares/auth')
+const isAdmin = require('../middlewares/isAdmin')
 
 router.get("/users", userController.getUsers)
 router.get('/users/:id', userController.getUserByID)
 router.post('/users', upload.none(), userController.postUser)
-router.put('/users/:id', upload.none(), userController.editUser)
-router.delete('/user/:id', userController.deleteUser)
-router.post('/login', userController.logIn)
+router.put('/users/:id', [ auth, upload.none() ] , userController.editUser)
+router.delete('/user/:id', [ auth, isAdmin ] , userController.deleteUser)
+router.post('/login', upload.none() , userController.logIn)
 
 module.exports = router
